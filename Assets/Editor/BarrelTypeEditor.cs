d9d0c2a712ed2c5c4a78c9fc4296a58bd42cf57f
kychris <kychris1999@hotmail.com>
2021-07-13 21:33:34 -0700

    Learning serialization and serializable field stuff

--- Assets/Editor/BarrelTypeEditor.cs
+++ Assets/Editor/BarrelTypeEditor.cs
@@ -6,9 +6,36 @@ using UnityEditor;
 [CustomEditor(typeof(BarrelType))]
 public class BarrelTypeEditor : Editor
 {
+    public float thing0; // serialized, visible, public
+    float thing1; //not serialed, hidden, private
+    [SerializeField] float thing2; //serialized, visible, private
+    [HideInInspector] public float thing3; //serialized, hidden, public
+
+    public enum Things
+    {
+        Bleep, Bloop, Blap
+    }
+
+    Things things;
+    float someValue;
+
+
     public override void OnInspectorGUI()
     {
-        base.OnInspectorGUI();
+        GUILayout.Label("test");
+        if (GUILayout.Button("Do a thing"))
+            Debug.Log("did a thing");
+
+        things = (Things)EditorGUILayout.EnumPopup(things);
+
+
+        // explicit positioning using rect
+        // GUI
+        // EditorGUI
+
+        // implicit positioning, auto-layout
+        // GUILayout
+        // EditorGUILayout
     }
 }
 
--- Assets/Scripts/BarrelType.cs
+++ Assets/Scripts/BarrelType.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,9 +8,24 @@ public class BarrelType : ScriptableObject
 {
     [Range(1f, 8f)]
     public float radius = 1f;
-
     public float damage = 10;
-
     public Color color = Color.red;
+    public List<MyClass> test;
 
+    public ChildClass test2;
+}
+
+[Serializable]
+public class MyClass
+{
+    public Vector3 pos;
+    public Color color;
+}
+
+// Serialize do not work with polymorphism, list mix of parent and children, 
+// extra attributes are dropped.
+[Serializable]
+public class ChildClass : MyClass
+{
+    public Quaternion rot;
 }

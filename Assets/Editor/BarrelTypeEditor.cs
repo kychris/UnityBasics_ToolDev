a33219fa4a67d2d4d59b37e1ebafd84dad24d238
kychris <kychris1999@hotmail.com>
2021-07-20 08:00:26 -0700

    Bad way of implementing editor

--- Assets/Editor/BarrelTypeEditor.cs
+++ Assets/Editor/BarrelTypeEditor.cs
@@ -2,56 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.ComponentModel;
 
+[CanEditMultipleObjects]
 [CustomEditor(typeof(BarrelType))]
 public class BarrelTypeEditor : Editor
 {
-    public float thing0; // serialized, visible, public
-    float thing1; //not serialed, hidden, private
-    [SerializeField] float thing2; //serialized, visible, private
-    [HideInInspector] public float thing3; //serialized, hidden, public
-
-    public enum Things
-    {
-        Bleep, Bloop, Blap
-    }
-
-    Things things;
-    float someValue;
-
-    Transform tf;
-
-
     public override void OnInspectorGUI()
     {
-        using (new GUILayout.VerticalScope(EditorStyles.helpBox))
+        // No undo, doesn't save when existing unity, no multiple object support
+        BarrelType barrel = target as BarrelType;
+        float newRadius = EditorGUILayout.FloatField("radius", barrel.radius);
+        if (newRadius != barrel.radius)
         {
-            using (new GUILayout.HorizontalScope())
-            {
-                // GUILayout.BeginHorizontal();
-                GUILayout.Label("Title", new GUILayoutOption[] { GUILayout.Width(50) });
-                if (GUILayout.Button("Do a thing"))
-                    Debug.Log("did a thing");
-                things = (Things)EditorGUILayout.EnumPopup(things);
-                // GUILayout.EndHorizontal();
-            }
-
-            GUILayout.Label("things", EditorStyles.toolbarButton);
-            GUILayout.Label("things", GUI.skin.button);
+            Undo.RecordObject(barrel, "change barrel radius");
+            barrel.radius = newRadius;
         }
-        GUILayout.Space(40);
-        GUILayout.Label("Things", EditorStyles.boldLabel);
 
-        EditorGUILayout.ObjectField("Assign here: ", null, typeof(Transform), true);
+        barrel.damage = EditorGUILayout.FloatField("damage", barrel.damage);
+        barrel.color = EditorGUILayout.ColorField("color", barrel.color);
 
-
-        // explicit positioning using rect
-        // GUI
-        // EditorGUI
-
-        // implicit positioning, auto-layout
-        // GUILayout
-        // EditorGUILayout
     }
 }
 
